2d24d931f14ad4eebd94df06c7b2b0c6d86a6191
Crevan <a.siverskii@gmail.com>
2023-03-25 23:15:29 +0300

    initial commit

---
+++ src/main/java/com/crevan/restemployeemanagment/RestEmployeeManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.crevan.restemployeemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEmployeeManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestEmployeeManagmentApplication.class, args);
+    }
+
+}
